f91064f043581d01b6d9f4a0882f4bec7e94614a
Manuel Morini <manuel.morini@dxc.com>
2024-03-28 11:31:48 +0100

    log + bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,7 +50,7 @@ fi
 printf "Executing GitHub Runner for $GITHUB_REPOSITORY\n"
 
 if [[ -n $ECS_TASK_MAX_DURATION_SECONDS ]]; then
-	sh /home/github/stopRunnerProcess.sh "run.sh" &
+	. /home/github/stopRunnerProcess.sh "run.sh" &
 fi
 
 . /actions-runner/run.sh
--- stopRunnerProcess.sh
+++ stopRunnerProcess.sh
@@ -5,5 +5,7 @@ sleep $ECS_TASK_MAX_DURATION_SECONDS
 PROCESS_TO_FIND=$1
 PID_TO_KILL=$(ps -eux | grep -v "grep" | grep "$PROCESS_TO_FIND" | tr -s ' ' | cut -d ' ' -f 2)
 
-echo "Stop process $PROCESS_TO_FIND with PID: $PID_TO_KILL"
+echo "Task timeout" >&2
+echo "Stop process $PROCESS_TO_FIND with PID: $PID_TO_KILL" >&2
+
 kill $PID_TO_KILL
