a8bec8b859e356e17fb0eb32d1098f6dbf9242fe
Manuel Morini <manuel.morini@dxc.com>
2024-03-28 12:37:33 +0100

    force kill

--- stopRunnerProcess.sh
+++ stopRunnerProcess.sh
@@ -8,4 +8,4 @@ PID_TO_KILL=$(ps -eux | grep -v "grep" | grep "$PROCESS_TO_FIND" | tr -s ' ' | c
 echo "Task timeout" >&2
 echo "Stop process $PROCESS_TO_FIND with PID: $PID_TO_KILL" >&2
 
-kill $PID_TO_KILL
+kill -9 $PID_TO_KILL
