25abb6e7fd0a8f18104df6bb85802adb24ccbc76
Manuel Morini <manuel.morini@dxc.com>
2024-04-05 16:17:31 +0200

    pid graceful stop

--- killProcess.sh
+++ killProcess.sh
@@ -3,9 +3,13 @@
 sleep $ECS_TASK_MAX_DURATION_SECONDS
 
 PROCESS_TO_FIND=$1
-PID_TO_KILL=$(ps -eux | grep -v "grep" | grep "$PROCESS_TO_FIND" | tr -s ' ' | cut -d ' ' -f 2)
+PROCESS_TO_KILL=$(ps -F | grep -v "grep" | grep "$PROCESS_TO_FIND" | tr -s ' ')
+PID_TO_KILL=$(ps -F | grep -v "grep" | grep "$PROCESS_TO_FIND" | tr -s ' ' | cut -d ' ' -f 2)
 
 echo "Task timeout" >&2
 echo "Stop process $PROCESS_TO_FIND with PID: $PID_TO_KILL" >&2
+echo "Process name: $PROCESS_TO_KILL"
 
+kill -s TERM $PID_TO_KILL
+sleep 30s
 kill -9 $PID_TO_KILL
