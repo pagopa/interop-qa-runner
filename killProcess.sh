9d8f252051ff054490f0c42cd9ffaec6a41ed22f
Manuel Morini <manuel.morini@dxc.com>
2024-04-08 15:24:29 +0200

    feat: ps eux

--- killProcess.sh
+++ killProcess.sh
@@ -3,8 +3,8 @@
 sleep $ECS_TASK_MAX_DURATION_SECONDS
 
 PROCESS_TO_FIND=$1
-PROCESS_TO_KILL=$(ps -F | grep -v "grep" | grep "$PROCESS_TO_FIND" | tr -s ' ')
-PID_TO_KILL=$(ps -F | grep -v "grep" | grep "$PROCESS_TO_FIND" | tr -s ' ' | cut -d ' ' -f 2)
+PROCESS_TO_KILL=$(ps -eux | grep -v "grep" | grep "$PROCESS_TO_FIND" | tr -s ' ')
+PID_TO_KILL=$(ps -eux | grep -v "grep" | grep "$PROCESS_TO_FIND" | tr -s ' ' | cut -d ' ' -f 2)
 
 echo "Task timeout" >&2
 echo "Stop process $PROCESS_TO_FIND with PID: $PID_TO_KILL" >&2
